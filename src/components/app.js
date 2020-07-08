d40b7ec1997415ef6acad25a286f6f07069de875
kaylynfletcher <efaquietshadow@gmail.com>
2020-07-07 22:35:52 -0600

    added random color

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import RandomColor from "./random-color";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <RandomColor />
       </div>
     );
   }
